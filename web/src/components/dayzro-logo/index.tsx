86456d0c72076004adec57d3ee77b02f4906a1ce
Wangsit Nursyahada <wangsitsada1234@gmail.com>
2026-09-25 23:58:35 +0700

    feat(web): add a large size to the Dayzro logo lockup

--- web/src/components/dayzro-logo/index.tsx
+++ web/src/components/dayzro-logo/index.tsx
@@ -5,10 +5,12 @@ import css from "./dayzro-logo.module.scss";
 
 type Props = {
     className?: string;
+    /** "md" for the navbar, "lg" for the footer brand block. */
+    size?: "md" | "lg";
 };
 
-export const DayzroLogo: React.FC<Props> = ({ className }) => (
-    <span className={clsx(css.logo, className)}>
+export const DayzroLogo: React.FC<Props> = ({ className, size = "md" }) => (
+    <span className={clsx(css.logo, size === "lg" && css._lg, className)}>
         <DayzroMark size={30} className={css.logo_mark} />
         <span className={css.logo_text}>Dayzro</span>
     </span>
